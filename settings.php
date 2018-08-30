1ab245bf2c66e7d60f876d88d7206598f951bd64
franschaniago <franschaniago5@gmail.com>
2018-08-30 13:21:42 +0700

    first

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '29955545384-5867jgbdl5j0jvat24ki579etcqg8b4i.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'izmbi-WWQHAVjbneZHlSbbKh');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/glogin/gauth.php');
+
+?>
