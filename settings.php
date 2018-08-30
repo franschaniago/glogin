c58e68bbd495bd57ba00a5d90cf336614b83df38
Frans Chaniago <franschaniago5@gmail.com>
2018-08-30 13:23:07 +0700

    update settings.php

--- settings.php
+++ settings.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Google App Client Id */
-define('CLIENT_ID', '29955545384-5867jgbdl5j0jvat24ki579etcqg8b4i.apps.googleusercontent.com');
+define('CLIENT_ID', '');
 
 /* Google App Client Secret */
-define('CLIENT_SECRET', 'izmbi-WWQHAVjbneZHlSbbKh');
+define('CLIENT_SECRET', '');
 
 /* Google App Redirect Url */
 define('CLIENT_REDIRECT_URL', 'http://localhost/glogin/gauth.php');
